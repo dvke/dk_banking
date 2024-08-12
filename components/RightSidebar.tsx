0fb6c5558646db6b27989a16ed4935b87ecad487
Uche Obiekwe <obiekweuchenna@gmail.com>
2024-08-12 13:12:39 +0100

    Implement complete layout

--- components/RightSidebar.tsx
+++ components/RightSidebar.tsx
@@ -41,7 +41,7 @@ const RightSidebar = ({ user, transactions, banks }: RightSidebarProps) => {
               />
             </div>
             {banks[1] && (
-              <div className="absolute right-0 top-8 z-0 w-[90%]">
+              <div className="absolute left-3 top-7 z-0 w-[100%]">
                 <BankCard
                   key={banks[1].id}
                   account={banks[1]}
